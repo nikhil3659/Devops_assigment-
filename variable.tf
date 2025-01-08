0dff3d1d8a1f9f1eccb7ee52f9ab80423baae557
nikhil3659 <nikhil.mourya@nashtechglobal.com>
2025-01-08 13:15:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "Myaccess_Id" {
-    default = "AKIAXZ5NF62GDZKMGKKU"
+    default = ""
     type = "string"
     
     }
 variable "Mysecrate_key" {
-    default = "5i74/gL+MWsmMAezZzMy+s4EIvH7R4tdTVnvbq1C"
+    default = ""
   }
